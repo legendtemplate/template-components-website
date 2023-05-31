c3ab957d429792cfceea99486e89976968b1c9af
528982tts <mmuzammiloff@gmail.com>
2023-05-31 09:18:49 +0500

    updated : 09:11 31/05/2023

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/sitemap.xml',
-                destination: '/api/sitemap'
-            }
-        ];
-    }
+   
 }
 
 module.exports = nextConfig
