032a3814ddc03e42814a36324dee6cef892529c7
528982tts <mmuzammiloff@gmail.com>
2023-05-31 08:48:51 +0500

    updated : 08:48 31/05/2023

--- app/page.tsx
+++ app/page.tsx
@@ -17,36 +17,6 @@ export const metadata: Metadata = {
   keywords: ['Next.js', 'React', 'JavaScript'],
   metadataBase: new URL(`${data.Url}`),
 
-  
-    openGraph: {
-      title: `${data.title}`,
-      description: `${data.title}`,
-      url: `${data.Url}`,
-      siteName: 'Next.js',
-      images: [
-        {
-          url: 'https://nextjs.org/og.png',
-          width: 800,
-          height: 600,
-        },
-        {
-          url: 'https://nextjs.org/og-alt.png',
-          width: 1800,
-          height: 1600,
-          alt: 'My custom alt',
-        },
-      ],
-      locale: 'en_US',
-      type: 'website',
-    },
-    robots: {
-      index: true,
-      follow: true,
-      googleBot: {
-        index: true,
-        follow: true,
-      },
-    },
 }
 export default function Home() {
   return (
