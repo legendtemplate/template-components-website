6eaa1763488586ca3317b96ac3deb4068c65f54a
528982tts <mmuzammiloff@gmail.com>
2023-06-05 15:34:15 +0500

    updated : 15:30 05/06/2023

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,30 +12,11 @@ const data = {
   Url : 'https://acme.com'
 }
 export const metadata:Metadata = {
-  colorScheme: "light",  // not change
+  colorScheme: "dark",  // not change
   creator: "Jiachi Liu",  // not change
   publisher: "Sebastian Markbåge", // not change
   themeColor: "black", // not change
-  openGraph: {
-    siteName: 'Next.js',
-  
-    locale: 'en_US',
-    type: 'website',
-  },
-  icons: {
-    icon: [{ url: '/icon.png' }, new URL('/icon.png', 'https://example.com')],
-    shortcut: ['/shortcut-icon.png'],
-    apple: [
-      { url: '/apple-icon.png' },
-      { url: '/apple-icon-x3.png', sizes: '180x180', type: 'image/png' },
-    ],
-    other: [
-      {
-        rel: 'apple-touch-icon-precomposed',
-        url: '/apple-touch-icon-precomposed.png',
-      },
-    ],
-  },
+ 
 };
 
 export default function RootLayout({
@@ -53,7 +34,6 @@ export default function RootLayout({
           {children}
           <Footer />
         </Providers>
-  {/* </Provider> */}
       </body>
     </html>
   );
