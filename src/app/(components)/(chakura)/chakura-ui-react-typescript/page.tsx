d288af96773be860e9471b8f8da41847c5badba6
528982tts <mmuzammiloff@gmail.com>
2023-06-13 08:43:36 +0500

    Start updated : 08:43 13/06/2023

--- src/app/(components)/(chakura)/chakura-ui-react-typescript/page.tsx
+++ src/app/(components)/(chakura)/chakura-ui-react-typescript/page.tsx
@@ -1,35 +1,35 @@
-"use client";
-import Link from "next/link";
-import { useState } from "react";
+// "use client";
+// import Link from "next/link";
+// import { useState } from "react";
 
-import { Metadata } from "next";
+// import { Metadata } from "next";
 import { SmallHero, Card, Container } from "@/components/index";
-import Cards from "./data";
-import { Button } from "@chakra-ui/react";
-const imagePerRow = 3;
-const data = {
-  title: "HTML CSS and Js Components",
-  description:
-    "This website provide components and templates of react , next js and typescript support with tailwind css and chakura ui.",
-  Url: "https://acme.com",
-};
+// import Cards from "./data";
+// import { Button } from "@chakra-ui/react";
+// const imagePerRow = 3;
+// const data = {
+//   title: "HTML CSS and Js Components",
+//   description:
+//     "This website provide components and templates of react , next js and typescript support with tailwind css and chakura ui.",
+//   Url: "https://acme.com",
+// };
 
-export const metadata: Metadata = {
-  title: `${data.title}`,
-  description: `${data.description}`,
-  keywords: ["about us"],
-  metadataBase: new URL(`${data.Url}`),
-};
+// export const metadata: Metadata = {
+//   title: `${data.title}`,
+//   description: `${data.description}`,
+//   keywords: ["about us"],
+//   metadataBase: new URL(`${data.Url}`),
+// };
 
 export default function page() {
-  const [next, setNext] = useState(6);
-  const loadMorePets = () => {
-    setNext(next + imagePerRow);
-  };
+  // const [next, setNext] = useState(6);
+  // const loadMorePets = () => {
+  //   setNext(next + imagePerRow);
+  // };
   return (
     <>
       <SmallHero head={`${data.title}`} />
-      <Container>
+      {/* <Container>
         <div className="cards-list grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 mb-10">
           {Cards.slice(0, next).map((item: any) => (
             <Card key={item.id} data={item} />
@@ -41,7 +41,7 @@ export default function page() {
         onClick={loadMorePets}
       >
         Load More
-      </Button>
+      </Button> */}
     </>
   );
 }
